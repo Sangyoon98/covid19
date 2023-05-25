f5eeb81d5731c109ffe1aebb7deef5bd86ba5452
Sangyoon <qlrqod123123@naver.com>
2023-05-25 22:36:36 +0900

    feat: Naver Maps Api 연동 및 구현

--- app/src/main/java/com/covid19/presentation/view/MainActivity.kt
+++ app/src/main/java/com/covid19/presentation/view/MainActivity.kt
@@ -2,9 +2,15 @@ package com.covid19.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.covid19.R
 import com.covid19.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
@@ -12,10 +18,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        mapFragment.getMapAsync(this)
     }
 
     override fun onDestroy() {
         mBinding = null
         super.onDestroy()
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val marker = Marker()
+        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.map = naverMap
     }
 }
