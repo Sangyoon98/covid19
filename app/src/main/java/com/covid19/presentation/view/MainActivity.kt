4df355b4bcdafccf6ab6376db0264490c129ca9b
Sangyoon <qlrqod123123@naver.com>
2023-05-26 17:52:25 +0900

    feat: Naver Maps Marker 클릭 이벤트 구현 / 정보 안내창 구현

--- app/src/main/java/com/covid19/presentation/view/MainActivity.kt
+++ app/src/main/java/com/covid19/presentation/view/MainActivity.kt
@@ -3,22 +3,31 @@ package com.covid19.presentation.view
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.covid19.R
+import com.covid19.data.repository.CentersDatabase
 import com.covid19.databinding.ActivityMainBinding
 import com.covid19.presentation.viewmodel.MainActivityViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_COLLAPSED
+import com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_EXPANDED
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
     private val viewModel: MainActivityViewModel by viewModels()
+    private val roomDatabase: CentersDatabase = CentersDatabase.getInstance(this)!!
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +41,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
         mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
     }
 
     override fun onDestroy() {
@@ -40,6 +51,67 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(naverMap: NaverMap) {
-        viewModel.setMarker(naverMap)
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        bottomSheetBehavior.state = STATE_COLLAPSED
+
+        lifecycleScope.launch {
+            val result = roomDatabase.centersDao().getAll()
+
+            result.forEach {centers ->
+                val marker = Marker()
+                marker.position = LatLng(centers.lat.toDouble(), centers.lng.toDouble())
+                if (centers.centerType == "중앙/권역")
+                    marker.iconTintColor = Color.BLUE
+                else
+                    marker.iconTintColor = Color.GREEN
+                marker.map = naverMap
+                Log.d("test", "$centers")
+
+                marker.setOnClickListener {
+                    binding.address.text = centers.address
+                    binding.centerName.text = centers.centerName
+                    binding.facilityName.text = centers.facilityName
+                    binding.phoneNumber.text = centers.phoneNumber
+                    binding.updatedAt.text = centers.updatedAt
+
+                    val cameraUpdate = CameraUpdate
+                        .scrollAndZoomTo(marker.position, 15.0)
+                        .animate(CameraAnimation.Easing)
+                    naverMap.moveCamera(cameraUpdate)
+                    Log.d("test", "marker clicked")
+
+                    if (bottomSheetBehavior.state == STATE_EXPANDED) {
+                        bottomSheetBehavior.state = STATE_COLLAPSED
+                    } else {
+                        bottomSheetBehavior.state = STATE_EXPANDED
+                    }
+
+                    false
+                }
+            }
+        }
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
